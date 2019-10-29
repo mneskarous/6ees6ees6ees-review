eaa112aae92c7de371e314919cd4be43e835087f
Michael Eskarous <mneskarous@gmail.com>
2019-10-29 10:08:26 -0700

    Added age property to class

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,6 +1,7 @@
-class ForagerBee extends Bee{
+class ForagerBee extends Bee {
   constructor() {
     super();
+    this.age = 10;
     this.job = 'find pollen';
     this.canFly = true;
     this.treasureChest = [];
