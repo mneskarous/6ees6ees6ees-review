4cb939dac014f7e30aaac348ae607cc8f098a2e3
Michael Eskarous <mneskarous@gmail.com>
2019-10-29 10:03:10 -0700

    Created HoneyMakerBee class in ES6

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,8 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
 };
